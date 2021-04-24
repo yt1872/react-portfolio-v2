51432f5d369f287c123ca37136882e15167cb6f5
Yuichiro Toyama <ytoyama1872@gmail.com>
2021-04-24 15:54:25 +0900

    add svg and header

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,25 +1,29 @@
 import React, { Component } from "react";
 import './hero.scss';
+import './programmer.js'
+import Programmer from "./programmer.js";
 // import '../index.scss'
 
 class Hero extends React.Component {
     render () {
-        return <div className="hero-container">
-                    <div className="hero-left">
-                        <div className="hero-name">
-                            Hi, I am Yuichiro Toyama.
-                        </div>
-                        <div className="hero-name">
-                            A FullStack Developer based in Tokyo.
-                        </div>
-                        <div className='hero-intro'>
-                            I design and build interactive and responsive websites.
-                        </div>
+        return(
+            <div className="hero-container">
+                <div className="hero-left">
+                    <div className="hero-name">
+                        Hi, I am Yuichiro Toyama.
                     </div>
-                    <div>
-                        
+                    <div className="hero-name">
+                        A FullStack Developer based in Tokyo.
+                    </div>
+                    <div className='hero-intro'>
+                        I design and build interactive and responsive websites.
                     </div>
                 </div>
+                <div>
+                    <Programmer />
+                </div>
+            </div>
+        ) 
     }
 }
 
